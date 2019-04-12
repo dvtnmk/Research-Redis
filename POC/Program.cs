ae742174a206cfa52d58001c357cbc97e01b958d
DvtNmk <devotion.nk@gmail.com>
2019-04-12 10:45:30 +0700

    Initial

---
+++ POC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace POC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            if(redis.IsConnected)
+            {
+                Console.WriteLine("IsConnected");
+            }
+            else
+            {
+                Console.WriteLine("Service is unavailable");
+            }
+        }
+    }
+}
